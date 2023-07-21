bb16cef989645eac0d388763f94eb0447511c03f
vaishali tiwari <136571044+maahi104@users.noreply.github.com>
2023-07-21 21:33:41 +0530

    Add files via upload

---
+++ josn.py
@@ -0,0 +1,34 @@
+import json
+{'name': 'Madhu', 
+  'dob': '23-11-1998', 
+  'height': '5.1', 
+  'city': 'Kota', 
+  'state': 'Rajasthan'
+  }
+{'name': 'Abhiram', 
+ 'dob': '12-7-1992', 
+ 'height': '5.2', 
+ 'city': 'Jaipur', 
+ 'state': 'Rajasthan'
+ }
+{'name': 'Swathi', 
+ 'dob': '19-11-1995', 
+ 'height': '4.9', 
+ 'city': 'Jodhpur', 
+ 'state': 'Rajasthan'
+ }
+{'name': 'Harika', 
+  'dob': '23-1-1997', 
+  'height': '4.8', 
+  'city': 'Ajmer', 
+  'state': 'Rajasthan'
+ }
+{'name': 'Dileep', 
+ 'dob': '29-10-1998', 
+ 'height': '4.9', 
+ 'city': 'Udaipur', 
+ 'state': 'Rajasthan'
+ }
+
+
+
